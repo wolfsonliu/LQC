5c003a767229648a1c7dc007172cf671f97dea38
Zhiheng Liu <wolfsonliu@live.com>
2026-08-21 22:45:03 +0800

    test: add unit tests for lqc.utils helpers

---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+import pytest
+
+from lqc import utils
+
+
+def test_convert_complement():
+    assert utils.convert_complement('acgtn-') == 'tgcan-'
+
+
+def test_convert_reverse_complement():
+    assert utils.convert_reverse_complement('aacg') == 'cgtt'
+    assert utils.convert_reverse_complement('gt-ag') == 'ct-ac'
+
+
+def test_check_cs_md_tag():
+    tags = [('cs', ':5'), ('MD', '5'), ('NM', 0)]
+    assert utils.check_cs_md_tag(tags) == ['cs', 'MD']
+
+
+def test_bam_or_sam():
+    assert utils.bam_or_sam('x.bam') == 'BAM'
+    assert utils.bam_or_sam('x.sam') == 'SAM'
+
+
+def test_bam_or_sam_rejects_unknown_extension():
+    with pytest.raises(AssertionError):
+        utils.bam_or_sam('x.txt')
